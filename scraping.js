43aed7eccc82f7323d753c56406f09e39a942839
andrian patrick <dreyn0721@gmail.com>
2021-03-06 11:20:21 +0800

    Update scraping.js

--- scraping.js
+++ scraping.js
@@ -35,6 +35,12 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
+
+  	var truncate_tbl = "DROP TABLE Shippers";
+  	con.query(truncate_tbl, function (err, result) {
+		if (err) throw err;
+
+	});
   
 });
 
